e6b097193348602da0f858e77b122979bebf4ab1
max <mahdirezaii490@gmail.com>
2023-05-02 18:44:07 +0330

    nvim config added

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,6 @@
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
